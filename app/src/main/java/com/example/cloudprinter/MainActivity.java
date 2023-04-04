60fb36e6d5e7b34162382599ead2d934f951479b
mohanad2000 <mohanad_tayeb@hotmail.com>
2023-04-04 04:19:45 +0300

    beginning

---
+++ app/src/main/java/com/example/cloudprinter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cloudprinter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void text_clicked(View view) {
+        Toast.makeText(this, "an activity starter", Toast.LENGTH_SHORT).show();
+    }
+}
