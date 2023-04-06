13ca0e60a70a30659eddbef7bfbaa567627cbf9f
mohanad2000 <mohanad_tayeb@hotmail.com>
2023-04-06 22:02:25 +0300

    changed the overall design to a professional one

--- app/src/main/java/com/example/cloudprinter/MainActivity.java
+++ app/src/main/java/com/example/cloudprinter/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.cloudprinter;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity extends AppCompatActivity {
     Intent mIntent;
+
+    Bitmap printer_background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
